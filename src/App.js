127ec05874f34e35fa9e226c0d87a5333cb27570
Jeremy <jerkku_astrom@hotmail.com>
2023-08-26 15:15:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import AddEvent from './components/AddEvent';
 
 function App() {
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddEvent />
     </div>
   );
 }
---
+++ src/components/AddEvent.js
@@ -0,0 +1,23 @@
+
+
+export default function AddEvent() {
+
+    
+
+
+return (
+    <div>
+        <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="text" name="email" />
+        </label>
+      </form>
+    </div>
+    );
+};
